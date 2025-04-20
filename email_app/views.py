fe4aac7e9226020881913fde0a877643d8f21383
Rajesh <grajesh2906@gmail.com>
2025-04-20 09:47:45 +0530

    Addded code to send one Email adn secury feactures like storing credentials in .evn and replicating the .env.example file to add own credientials in oreder to avoid security issues

--- email_app/views.py
+++ email_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
-# Create your views here.
+def send_simple_email(request):
+    subject = 'Test Email'
+    message = 'This is a test email.'
+    email_from = 'grajesh2906@gmail.com'  # You can use the EMAIL_HOST_USER variable here as well
+    recipient = ['grajesh2907@gmail.com']  # Replace with a valid email address
+    res = send_mail(subject, message, email_from, recipient)
+    return HttpResponse(f"Email sent successfully: {res}")
